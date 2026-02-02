f9b892c030f60a7bdd8e6ecabd08e163e68baf4e
zNot3 <mariodaniel.blanco@galileo.edu>
2026-02-01 23:05:06 -0600

    Fixed base and changed imports

--- Labs/Lab2 - Traffic Light Simulator/app/src/main/java/com/example/trafficlightsimulator/MainActivity.kt
+++ Labs/Lab2 - Traffic Light Simulator/app/src/main/java/com/example/trafficlightsimulator/MainActivity.kt
@@ -3,27 +3,30 @@ package com.example.trafficlightsimulator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.trafficlightsimulator.ui.theme.TrafficLightSimulatorTheme
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TrafficLightSimulatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.DarkGray
+                    ) {
+                        TrafficLightSimulator()
+                    }
                 }
             }
         }
@@ -31,17 +34,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TrafficLightSimulator() {}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TrafficLightSimulatorTheme {
-        Greeting("Android")
-    }
-}
+fun TrafficLightCircle(color: Color) {}
